53797370fb0bd5ccf424ebc9b87aec32cff469a8
Artemis Mokhtari <gusmokhtar@student.gu.se>
2025-05-22 11:21:30 +0200

    #35 Delete wifi_config.h

--- Read_sensor_readings/wifi_config.h
+++
@@ -1,5 +0,0 @@
-#pragma once                                  
-const char* ssid = "Galaxy A21s0D5C ";                  
-const char* password = "Wifi_2020";           
-const char* mqtt_server = "192.168.6.117";  
-
